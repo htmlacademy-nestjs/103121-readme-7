e3b43563e99b66705ee3331dd58d3df96e6e87a9
artem-trikashnyi <artem.trikashnyi@indriver.com>
2024-07-27 19:40:05 +0300

    Добавит валидацию для Account

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,22 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  AuthenticationValidateMessage,
+  NAME_MAX_LENGTH, NAME_MIN_LENGTH,
+  PASSWORD_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH
+} from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique address',
     example: 'user@user.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User login',
     example: 'Keks',
   })
+  @IsString({ message: AuthenticationValidateMessage.NameIsNotString })
+  @MinLength(NAME_MIN_LENGTH, { message: AuthenticationValidateMessage.NameMinLength })
+  @MaxLength(NAME_MAX_LENGTH, { message: AuthenticationValidateMessage.NameMaxLength })
   public login: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456'
   })
+  @IsString({ message: AuthenticationValidateMessage.PasswordIsNotString })
+  @MinLength(PASSWORD_MIN_LENGTH, { message: AuthenticationValidateMessage.PasswordMinLength })
+  @MaxLength(PASSWORD_MAX_LENGTH, { message: AuthenticationValidateMessage.PasswordMaxLength })
   public password: string;
 
   @ApiProperty({
--- project/libs/account/authentication/src/dto/login-user.dto.ts
+++ project/libs/account/authentication/src/dto/login-user.dto.ts
@@ -1,15 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  AuthenticationValidateMessage,
+  PASSWORD_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH
+} from '../authentication-module/authentication.constant';
 export class LoginUserDto {
   @ApiProperty({
     description: 'User uniq email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456'
   })
+  @IsString({ message: AuthenticationValidateMessage.PasswordIsNotString })
+  @MinLength(PASSWORD_MIN_LENGTH, { message: AuthenticationValidateMessage.PasswordMinLength })
+  @MaxLength(PASSWORD_MAX_LENGTH, { message: AuthenticationValidateMessage.PasswordMaxLength })
   public password: string;
 }
