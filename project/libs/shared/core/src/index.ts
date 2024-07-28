7b7b7bf63e82b63e5fcf6a05b1a2121af301d680
artem-trikashnyi <artem.trikashnyi@indriver.com>
2024-07-28 16:00:11 +0300

    Добавит PaginationResult, SortDirection

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -8,3 +8,5 @@ export { Comment } from './lib/types/comment.interface';
 export { Like } from './lib/types/like.interface';
 export { PostType } from './lib/types/post-type.enum';
 export { PostStatusType } from './lib/types/post-status-type.enum';
+export { SortDirection } from './lib/interfaces/sort-direction.interface';
+export { PaginationResult } from './lib/interfaces/pagination.interface';
