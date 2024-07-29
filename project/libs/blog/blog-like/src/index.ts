df013d31d1ff75eac361fe5164a5477df20ddebd
artem-trikashnyi <artem.trikashnyi@indriver.com>
2024-07-29 17:01:33 +0300

    Добавит BlogPostEntity

---
+++ project/libs/blog/blog-like/src/index.ts
@@ -0,0 +1,6 @@
+export { BlogLikeModule } from './blog-like-module/blog-like.module';
+export { BlogLikeEntity } from './blog-like-module/blog-like.entity';
+export { BlogLikeFactory } from './blog-like-module/blog-like.factory';
+export { BlogLikeService } from './blog-like-module/blog-like.service';
+
+export { LikeRdo } from './blog-like-module/rdo/like.rdo';
--- project/libs/shared/core/src/lib/types/post.interface.ts
+++ project/libs/shared/core/src/lib/types/post.interface.ts
@@ -1,19 +1,20 @@
 import { Comment } from './comment.interface';
+import { Like } from './like.interface';
 import { PostStatusType } from './post-status-type.enum';
 import { PostType } from './post-type.enum';
 
 export interface Post {
-  id?: string;
-  title?: string;
-  video?: string;
-  preview?: string;
-  text?: string;
-  quoteText?: string;
-  quoteAuthor?: string;
-  photo?: string;
-  link?: string;
-  description?: string;
-  tags?: string[];
+  id: string;
+  title: string;
+  video: string;
+  preview: string;
+  text: string;
+  quoteText: string;
+  quoteAuthor: string;
+  photo: string;
+  link: string;
+  description: string;
+  tags: string[];
   type: PostType;
   status: PostStatusType;
   createdAt?: Date;
@@ -21,4 +22,5 @@ export interface Post {
   updatedAt?: Date;
   userId: string;
   comments: Comment[];
+  likes: Like[];
 }
